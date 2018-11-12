6b4141409602d19b98edbdb54aaad35f9ca8668b
Andrius Kucinskas <kucinskas.andrius92@gmail.com>
2018-11-12 08:43:10 +0000

    Include checking it he key starts with base64

--- src/uofa/Encryption/EncryptionServiceProvider.php
+++ src/uofa/Encryption/EncryptionServiceProvider.php
@@ -4,19 +4,21 @@ namespace uofa\Encryption;
 
 use Illuminate\Support\ServiceProvider;
 
-
 class EncryptionServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->bindShared('encrypter', function ($app) {
+            if (Str::startsWith($key = $app['config']['app.key'], 'base64:')) {
+                $key = base64_decode(substr($key, 7));
+            }
             if ($app['config']->has('app.cipher')) {
                 return new Encrypter(
-                    $app['config']['app.key'],
+                    $key,
                     $app['config']['app.cipher']
                 );
             } else {
-                return new Encrypter($app['config']['app.key']);
+                return new Encrypter($key);
             }
         });
         $this->app->alias('encrypter', Encrypter::class);
